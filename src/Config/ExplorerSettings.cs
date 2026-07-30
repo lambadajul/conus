a3cd9c30528ee67ddb3c6df1d5d5e358c59c0106
Julien <lambadajul@proton.me>
2026-07-30 21:14:19 +0300

    Add ExplorerSettings class with default configurations

---
+++ src/Config/ExplorerSettings.cs
@@ -0,0 +1,12 @@
+namespace PlcTagExplorer.Config;
+
+public static class ExplorerSettings
+{
+    public const int MaxResults = 100;
+
+    public const bool IgnoreCase = true;
+
+    public const string DefaultExportFile = "tags.csv";
+
+    public const string Version = "1.0";
+}
